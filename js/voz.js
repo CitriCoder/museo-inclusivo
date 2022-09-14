76c938a2d533a596e1d8399308d119d33f42e748
Lareallau <lautarolaraignee@gmail.com>
2022-09-14 18:57:56 -0300

    fix(): Arreglo de idioma variable a español por defecto

--- js/voz.js
+++ js/voz.js
@@ -5,5 +5,11 @@ document.getElementById('voz_btn').addEventListener("click",()=>{
 });
 
 function falar(texto) {
-    speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
+    var msg = new SpeechSynthesisUtterance(texto);
+    var voices = window.speechSynthesis.getVoices();
+    msg.volume = 1; // 0 to 1
+    msg.rate = 0.7; // 0.1 to 10
+    msg.pitch = 1; //0 to 2
+    msg.lang = 'es-AR';
+    speechSynthesis.speak(msg);
 }
